3296a972a95008c285f53f660c83e5254b4184a2
Ashish Sahu <sk.ashish97@gmail.com>
2016-03-08 02:21:54 +0530

    updated sql database file lms.sql

--- lms.sql
+++ lms.sql
@@ -3,7 +3,7 @@
 -- http://www.phpmyadmin.net
 --
 -- Host: 127.0.0.1
--- Generation Time: Mar 07, 2016 at 12:03 PM
+-- Generation Time: Mar 07, 2016 at 09:31 PM
 -- Server version: 5.6.25
 -- PHP Version: 5.6.11
 
@@ -30,14 +30,15 @@ CREATE TABLE IF NOT EXISTS `admins` (
   `id` int(11) NOT NULL,
   `username` varchar(30) NOT NULL,
   `password` varchar(32) NOT NULL
-) ENGINE=InnoDB AUTO_INCREMENT=2 DEFAULT CHARSET=latin1;
+) ENGINE=InnoDB AUTO_INCREMENT=3 DEFAULT CHARSET=latin1;
 
 --
 -- Dumping data for table `admins`
 --
 
 INSERT INTO `admins` (`id`, `username`, `password`) VALUES
-(1, 'abhi', '1234');
+(1, 'abhi', '1234'),
+(2, 'ash', '1234');
 
 -- --------------------------------------------------------
 
@@ -51,7 +52,35 @@ CREATE TABLE IF NOT EXISTS `leavetypes` (
   `abbreviation` varchar(5) NOT NULL,
   `numLeaves` smallint(6) NOT NULL,
   `inclusions` set('monday','tuesday','wednesday','thursday','friday','saturday','sunday','holidays') NOT NULL
-) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+) ENGINE=InnoDB AUTO_INCREMENT=3 DEFAULT CHARSET=latin1;
+
+--
+-- Dumping data for table `leavetypes`
+--
+
+INSERT INTO `leavetypes` (`id`, `leaveName`, `abbreviation`, `numLeaves`, `inclusions`) VALUES
+(1, 'Casual Leave', 'CL', 15, 'monday,tuesday,wednesday,thursday,friday'),
+(2, 'Medical Leave', 'ML', 15, 'monday,tuesday,wednesday,thursday,friday');
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `usertypes`
+--
+
+CREATE TABLE IF NOT EXISTS `usertypes` (
+  `id` int(11) NOT NULL,
+  `type` varchar(100) NOT NULL,
+  `accessibleLeaves` varchar(500) NOT NULL
+) ENGINE=InnoDB AUTO_INCREMENT=3 DEFAULT CHARSET=latin1;
+
+--
+-- Dumping data for table `usertypes`
+--
+
+INSERT INTO `usertypes` (`id`, `type`, `accessibleLeaves`) VALUES
+(1, 'B.tech ', 'Casual Leave'),
+(2, 'M.tech', 'Casual Leave,Medical Leave');
 
 --
 -- Indexes for dumped tables
@@ -70,6 +99,12 @@ ALTER TABLE `leavetypes`
   ADD PRIMARY KEY (`id`);
 
 --
+-- Indexes for table `usertypes`
+--
+ALTER TABLE `usertypes`
+  ADD PRIMARY KEY (`id`);
+
+--
 -- AUTO_INCREMENT for dumped tables
 --
 
@@ -77,12 +112,17 @@ ALTER TABLE `leavetypes`
 -- AUTO_INCREMENT for table `admins`
 --
 ALTER TABLE `admins`
-  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT,AUTO_INCREMENT=2;
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT,AUTO_INCREMENT=3;
 --
 -- AUTO_INCREMENT for table `leavetypes`
 --
 ALTER TABLE `leavetypes`
-  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT;
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT,AUTO_INCREMENT=3;
+--
+-- AUTO_INCREMENT for table `usertypes`
+--
+ALTER TABLE `usertypes`
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT,AUTO_INCREMENT=3;
 /*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
 /*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
 /*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
