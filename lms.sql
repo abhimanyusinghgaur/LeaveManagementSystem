359bcb48db8f5de53108e1d6752cf064e9632c09
Abhimanyu Singh Gaur <abhimanyusinghgaur@gmail.com>
2016-03-23 18:53:20 +0530

    Updated database schema

--- lms.sql
+++ lms.sql
@@ -3,7 +3,7 @@
 -- http://www.phpmyadmin.net
 --
 -- Host: 127.0.0.1
--- Generation Time: Mar 07, 2016 at 11:23 PM
+-- Generation Time: Mar 23, 2016 at 02:20 PM
 -- Server version: 5.6.25
 -- PHP Version: 5.6.11
 
@@ -30,15 +30,30 @@ CREATE TABLE IF NOT EXISTS `admins` (
   `id` int(11) NOT NULL,
   `username` varchar(30) NOT NULL,
   `password` varchar(32) NOT NULL
-) ENGINE=InnoDB AUTO_INCREMENT=3 DEFAULT CHARSET=latin1;
+) ENGINE=InnoDB AUTO_INCREMENT=2 DEFAULT CHARSET=latin1;
 
 --
 -- Dumping data for table `admins`
 --
 
 INSERT INTO `admins` (`id`, `username`, `password`) VALUES
-(1, 'abhi', '1234'),
-(2, 'ash', '1234');
+(1, 'admin', 'admin');
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `leaverequests`
+--
+
+CREATE TABLE IF NOT EXISTS `leaverequests` (
+  `id` int(11) NOT NULL,
+  `Username` varchar(30) NOT NULL,
+  `LeaveType` varchar(50) NOT NULL,
+  `StartDate` date NOT NULL,
+  `EndDate` date NOT NULL,
+  `Reason` varchar(500) NOT NULL,
+  `Status` enum('Granted','Rejected','Pending','') NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=latin1;
 
 -- --------------------------------------------------------
 
@@ -52,15 +67,7 @@ CREATE TABLE IF NOT EXISTS `leavetypes` (
   `abbreviation` varchar(5) NOT NULL,
   `numLeaves` smallint(6) NOT NULL,
   `inclusions` set('monday','tuesday','wednesday','thursday','friday','saturday','sunday','holidays') NOT NULL
-) ENGINE=InnoDB AUTO_INCREMENT=3 DEFAULT CHARSET=latin1;
-
---
--- Dumping data for table `leavetypes`
---
-
-INSERT INTO `leavetypes` (`id`, `leaveName`, `abbreviation`, `numLeaves`, `inclusions`) VALUES
-(1, 'Casual Leave', 'CL', 15, 'monday,tuesday,wednesday,thursday,friday'),
-(2, 'Medical Leave', 'ML', 15, 'monday,tuesday,wednesday,thursday,friday');
+) ENGINE=InnoDB DEFAULT CHARSET=latin1;
 
 -- --------------------------------------------------------
 
@@ -71,20 +78,13 @@ INSERT INTO `leavetypes` (`id`, `leaveName`, `abbreviation`, `numLeaves`, `inclu
 CREATE TABLE IF NOT EXISTS `users` (
   `id` int(11) NOT NULL,
   `Name` varchar(40) NOT NULL,
-  `User_id` varchar(30) NOT NULL,
+  `Username` varchar(30) NOT NULL,
   `Email` varchar(30) NOT NULL,
   `Password` varchar(30) NOT NULL,
-  `Gender` varchar(15) NOT NULL,
+  `Gender` enum('male','female') NOT NULL,
   `Type` varchar(30) NOT NULL,
-  `Casual_Leave_left` int(11) NOT NULL
-) ENGINE=InnoDB AUTO_INCREMENT=2 DEFAULT CHARSET=latin1;
-
---
--- Dumping data for table `users`
---
-
-INSERT INTO `users` (`id`, `Name`, `User_id`, `Email`, `Password`, `Gender`, `Type`, `Casual_Leave_left`) VALUES
-(1, 'Ashish Sahu', 'B14CS009', 'sahu.2@iitj.ac.in', '1234', 'male', 'B.tech', 15);
+  `Leaves_Left` varchar(50) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=latin1;
 
 -- --------------------------------------------------------
 
@@ -96,15 +96,7 @@ CREATE TABLE IF NOT EXISTS `usertypes` (
   `id` int(11) NOT NULL,
   `type` varchar(100) NOT NULL,
   `accessibleLeaves` varchar(500) NOT NULL
-) ENGINE=InnoDB AUTO_INCREMENT=3 DEFAULT CHARSET=latin1;
-
---
--- Dumping data for table `usertypes`
---
-
-INSERT INTO `usertypes` (`id`, `type`, `accessibleLeaves`) VALUES
-(1, 'B.tech ', 'Casual Leave'),
-(2, 'M.tech', 'Casual Leave,Medical Leave');
+) ENGINE=InnoDB DEFAULT CHARSET=latin1;
 
 --
 -- Indexes for dumped tables
@@ -114,6 +106,12 @@ INSERT INTO `usertypes` (`id`, `type`, `accessibleLeaves`) VALUES
 -- Indexes for table `admins`
 --
 ALTER TABLE `admins`
+  ADD PRIMARY KEY (`id`);
+
+--
+-- Indexes for table `leaverequests`
+--
+ALTER TABLE `leaverequests`
   ADD PRIMARY KEY (`id`);
 
 --
@@ -142,22 +140,27 @@ ALTER TABLE `usertypes`
 -- AUTO_INCREMENT for table `admins`
 --
 ALTER TABLE `admins`
-  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT,AUTO_INCREMENT=3;
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT,AUTO_INCREMENT=2;
+--
+-- AUTO_INCREMENT for table `leaverequests`
+--
+ALTER TABLE `leaverequests`
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT;
 --
 -- AUTO_INCREMENT for table `leavetypes`
 --
 ALTER TABLE `leavetypes`
-  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT,AUTO_INCREMENT=3;
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT;
 --
 -- AUTO_INCREMENT for table `users`
 --
 ALTER TABLE `users`
-  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT,AUTO_INCREMENT=2;
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT;
 --
 -- AUTO_INCREMENT for table `usertypes`
 --
 ALTER TABLE `usertypes`
-  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT,AUTO_INCREMENT=3;
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT;
 /*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
 /*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
 /*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
