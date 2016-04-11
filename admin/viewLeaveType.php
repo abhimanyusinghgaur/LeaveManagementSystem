0bc30e1bf96d829d03c8462bb70f91bf74da9890
Abhimanyu Singh Gaur <abhimanyusinghgaur@gmail.com>
2016-04-11 23:42:57 +0530

    More user-friendly

--- admin/viewLeaveType.php
+++ admin/viewLeaveType.php
@@ -1,4 +1,5 @@
 <div class="row">
+	<span class"left">Note: Inclusions are weekdays. <div class="btn-floating orange center">H</div> is for Holidays.</span>
 	<a class="btn-floating btn-large waves-effect waves-light red right tooltipped" data-position="right" data-delay="50" data-tooltip="Add new leave type" href="index.php?id=addLeaveType"><i class="material-icons">add</i></a>
 </div>
 <div class="row">
@@ -29,7 +30,16 @@
 						'<td>'.$objLeaveType->getLeaveName().'</td>'.
 						'<td>'.$objLeaveType->getAbbreviation().'</td>'.
 						'<td>'.$objLeaveType->getNumLeaves().'</td>'.
-						'<td>'.$objLeaveType->getInclusions().'</td>'.
+						'<td>'.
+							'<div class="btn-floating orange center'; if(strpos($objLeaveType->getInclusions(), 'Mon') === false) echo ' disabled'; echo '">M</div>'.
+							'<div class="btn-floating orange center'; if(strpos($objLeaveType->getInclusions(), 'Tue') === false) echo ' disabled'; echo '">T</div>'.
+							'<div class="btn-floating orange center'; if(strpos($objLeaveType->getInclusions(), 'Wed') === false) echo ' disabled'; echo '">W</div>'.
+							'<div class="btn-floating orange center'; if(strpos($objLeaveType->getInclusions(), 'Thu') === false) echo ' disabled'; echo '">T</div>'.
+							'<div class="btn-floating orange center'; if(strpos($objLeaveType->getInclusions(), 'Fri') === false) echo ' disabled'; echo '">F</div>'.
+							'<div class="btn-floating orange center'; if(strpos($objLeaveType->getInclusions(), 'Sat') === false) echo ' disabled'; echo '">S</div>'.
+							'<div class="btn-floating orange center'; if(strpos($objLeaveType->getInclusions(), 'Sun') === false) echo ' disabled'; echo '">S</div>'.
+							'<div class="btn-floating orange center'; if(strpos($objLeaveType->getInclusions(), 'Holidays') === false) echo ' disabled'; echo '">H</div>'.
+						'</td>'.
 					'</tr>';
 				$i++;
 			}
